2aa32bff958508376e1449229052835d11c09a4e
Batanai <batiemc@gmail.com>
2021-01-11 20:36:36 +0200

    UberOptionsCommit

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { StyleSheet, Text, View, StatusBar } from "react-native";
 import DestinationSearch from "./src/screens/DestinationSearch";
 import HomeScreen from "./src/screens/HomeScreen";
+import SearchResults from "./src/screens/SearchResults";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar barStyle="dark-content" />
       {/* <HomeScreen /> */}
-      <DestinationSearch />
+      {/* <DestinationSearch /> */}
+      <SearchResults />
     </View>
   );
 }
